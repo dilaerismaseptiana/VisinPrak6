e1bbc5df016bd4179b97378488d9faf543e0f8dd
dilaerismaseptiana <dlarsmsptna7743@gmail.com>
2023-06-19 21:37:49 +0700

    .

--- dashboard.js
+++ dashboard.js
@@ -3,7 +3,7 @@ google.charts.setOnLoadCallback(getSpreadsheetData);
 
 function getSpreadsheetData() {
     var spreadsheetId = '1kh865J7uGpI_FglAOGmOFKGGZatCn6Cc5oi-0l_0vgE';
-    var range = 'TS!A1:B13';
+    var range = 'TS!A16:C20';
 
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/' + spreadsheetId + '/gviz/tq?gid=0&range=' + range);
     query.send(handleQueryResponse);
@@ -21,7 +21,25 @@ function handleQueryResponse(response) {
 
 function drawCharts(data) {
     var options = {
-        title: 'Judul Grafik',
+        title: 'Perbandingan Jumlah Jenis makanan dan Total Harga dalam Satu Tahun',
+        width: 400,
+        height: 300
+    };
+
+    var options1 = {
+        title: 'Overtime berdasarkan kategori makanan ',
+        width: 400,
+        height: 300
+    };
+
+    var options2 = {
+        title: 'Presentase Kategori Makanan',
+        width: 400,
+        height: 300
+    };
+
+    var options3 = {
+        title: 'Urutan Kategori Makanan Berdasarkan Quantity dan Total Price',
         width: 400,
         height: 300
     };
@@ -29,14 +47,14 @@ function drawCharts(data) {
     var chart1 = new google.visualization.ColumnChart(document.getElementById('chart1'));
     chart1.draw(data, options);
 
-    var chart2 = new google.visualization.PieChart(document.getElementById('chart2'));
-    chart2.draw(data, options);
+    var chart2 = new google.visualization.LineChart(document.getElementById('chart2'));
+    chart2.draw(data, options1);
 
-    var chart3 = new google.visualization.LineChart(document.getElementById('chart3'));
-    chart3.draw(data, options);
+    var chart3 = new google.visualization.PieChart(document.getElementById('chart3'));
+    chart3.draw(data, options2);
 
-    var chart4 = new google.visualization.BarChart(document.getElementById('chart4'));
-    chart4.draw(data, options);
+    var chart4 = new google.visualization.AreaChart(document.getElementById('chart4'));
+    chart4.draw(data, options3);
 
     // Tambahkan jenis grafik lain dan elemen div untuk grafik lainnya
 }
